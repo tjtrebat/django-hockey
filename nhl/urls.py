6ceff62a0528213f463e748465ef2919afda90f6
Tom Trebat <tjtrebat@gmail.com>
2011-11-18 11:49:13 -0500

    started schedule dialog

--- nhl/urls.py
+++ nhl/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
     url(r'^roster/(?P<team_id>\d+)$', 'nhl.views.roster', name='roster'),
+    url(r'^schedule/(?P<team_id>\d+)$', 'nhl.views.schedule', name='schedule'),
 )
--- nhl/views.py
+++ nhl/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import *
-from nhl.models import Player
+from django.db.models import Q
+from nhl.models import *
 
-def roster(request, team_id, template="nhl/_roster.html"):
+def roster(request, team_id, template="nhl/roster.html"):
     return render(request, template, {"players": get_list_or_404(Player, pk=team_id)})
+
+def schedule(request, team_id, template="nhl/schedule.html"):
+    return render(request, template, {"games": get_list_or_404(Game, Q(home=team_id) | Q(visitor=team_id))})
