bc1586cd74d5231fbd80fc9a94e49fa002aee61b
Tom Trebat <tjtrebat@gmail.com>
2011-11-24 11:39:00 -0500

    added height option to dialog

--- nhl/views.py
+++ nhl/views.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from nhl.models import *
 
 def roster(request, team_id, template="nhl/roster.html"):
-    return render(request, template, {"players": get_list_or_404(Player, pk=team_id)})
+    return render(request, template, {"players": get_list_or_404(Player, team__pk=team_id)})
 
 def schedule(request, team_id, template="nhl/schedule.html"):
     return render(request, template, {"games": get_list_or_404(Game, Q(home=team_id) | Q(visitor=team_id))})
